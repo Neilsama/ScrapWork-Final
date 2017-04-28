76ab3b10e274c2c8c5125102c3474ec7a41f792c
Neilsama <neilsamanyc@gmail.com>
2017-04-28 13:47:09 -0400

    fixed patch class mouse down and up events

--- ScrapWork/src/ScrapWorkApp.cpp
+++ ScrapWork/src/ScrapWorkApp.cpp
@@ -22,8 +22,8 @@ using namespace std;
 class ScrapWorkApp : public App {
   public:
 	void setup() override;
-	void mouseDown( MouseEvent event ) override;
-    void mouseUp( MouseEvent event )  override;
+    void getPosInCanvas(int i, int j );
+    void mouseUp(ci::app::MouseEvent event) override;
 	void update() override;
 	void draw() override;
     
@@ -37,11 +37,11 @@ class ScrapWorkApp : public App {
     po::scene::ImageRef             bgPImg; // background image
     SelectPatchPanelRef             mSelectPatchPanel; //  select patch panel image
     PreviewPanelRef                 mPreviewPanel; //  preview panel
-    CanvasRef                          mCanvas; // canvas image
+    CanvasRef                       mCanvas; // canvas image
     
     
-    std::vector<int>           patchesQueue; //all patches already add in play panel
-    PatchRef                        newPatch;
+    std::vector<PatchRef>           patchesQueue; //all id of patches that already add in canvas
+    PatchRef                        newPatch; // when click on patch in grid, will generate a new patch
 };
 
 void ScrapWorkApp::setup()
@@ -75,107 +75,81 @@ void ScrapWorkApp::setup()
 
 void ScrapWorkApp::generateNewPatch(int number)
 {
-    cout<<"generate a new patch"<<endl;
-    newPatch = Patch::create(mSelectPatchPanel->getPatch(number)->getTexture());
-    newPatch->setAlpha(0.f);
-//    newPatch->setPosition(mSelectPatchPanel->getPatch(number)->getPosition()-ci::vec2(-10));
-    ci::app::timeline().apply(&newPatch->getPositionAnim(), mSelectPatchPanel->getPatch(number)->getPosition(), mSelectPatchPanel->getPatch(number)->getPosition()-ci::vec2(-10), 0.2f, EaseInAtan());
-     ci::app::timeline().apply(&newPatch->getAlphaAnim(), 0.f, 1.f, 0.2f, EaseInAtan());
-    ci::app::timeline().apply(&newPatch->getScaleAnim(), ci::vec2(0.f), ci::vec2(2.f), 0.2f, EaseInAtan());
-   
-    newPatch->setAsNew(true);
-    newPatch->setID(mSelectPatchPanel->getPatchNum()+patchesQueue.size()+1);
-    activeContainer->addChild(newPatch);
+    cout<<"emiting signal patch ID is: "<<number<<endl;
+    if (number < 24) {
+        newPatch = Patch::create(mSelectPatchPanel->getPatch(number)->getTexture());
+        newPatch->setAlpha(0.f);
+        ci::app::timeline().apply(&newPatch->getPositionAnim(), mSelectPatchPanel->getPatch(number)->getPosition(), mSelectPatchPanel->getPatch(number)->getPosition()-ci::vec2(-10), 0.2f, EaseInAtan());
+        ci::app::timeline().apply(&newPatch->getAlphaAnim(), 0.f, 1.f, 0.2f, EaseInAtan());
+        ci::app::timeline().apply(&newPatch->getScaleAnim(), ci::vec2(0.f), ci::vec2(2.f), 0.2f, EaseInAtan());
+        
+        newPatch->setAsNew(true);
+        newPatch->setID(mSelectPatchPanel->getPatchNum()+patchesQueue.size());
+        activeContainer->addChild(newPatch);
+        
+        newPatch->getIsInCavasSignal().connect(std::bind(&ScrapWorkApp::showOnCanvas, this,std::placeholders::_1));
+        newPatch->getNewPatchSignal().connect(std::bind(&ScrapWorkApp::generateNewPatch,this, std::placeholders::_1));
+        //cout<<"generate a new patch, id is: "<<newPatch->getID()<<endl;
+    }
+    else
+    {
+        for (int i = 0; i < patchesQueue.size(); i++) {
+            cout<<"patchQuere["<<i<<"] id : "<<patchesQueue[i]->getID()<<endl;
+            if (patchesQueue[i]->getID() == number) {
+                newPatch = patchesQueue[i];
+                cout<<"not generate new one, newPatch ID is : "<<newPatch->getID()<<endl;
+            }
+        }
+    }
     
-    newPatch->getIsInCavasSignal().connect(std::bind(&ScrapWorkApp::showOnCanvas, this,std::placeholders::_1));
 
 }
 
 void ScrapWorkApp::showOnCanvas(bool state)
 {
-    if (state) {
-        if(newPatch->getIsNew()){
-            patchesQueue.push_back(newPatch->getID());
+    if (state) { // move new patch in, show on canvas
+        if(newPatch->getIsNew()){ // if it's the new generated patch
+            patchesQueue.push_back(newPatch);
+            newPatch->setAsNew(false);
             activeContainer->removeChild(newPatch);
             mCanvas->addChild(newPatch);
-            cout<<"add a new patch:"<<newPatch->getID()<<endl;
-            cout<<" now the size of patch queue is: "<<patchesQueue.size()<<endl;
-            //cout<<"showOnCanvas signal state : "<<state<<endl;
+            //getPosInCanvas(std::floor((newPatch->getPosition().x - 426)/100) , std::floor((newPatch->getPosition().y - 295)/100));
+            
+//            newPatch->setDrawBounds(true);
+//            newPatch->setAlignment(po::scene::Alignment::TOP_LEFT);
+//            cout <<"patch position is: "<<newPatch->getPosition() - mCanvas->getSize()-mCanvas->getPosition() - ci::vec2(0,180)<<endl;
+//            cout << "position is : "<<std::floor((newPatch->getPosition().x - mCanvas->getPosition().x - mCanvas->getSize().x)/100) <<", "<<std::floor((newPatch->getPosition().y - 295)/100)<<endl;
+            
         }
-        else
-            cout<<"still the patch in canvas"<<endl;
-    }else{
-        //cout<<"out of canvas"<<endl;
-        if(newPatch->getIsNew())
+    
+    }else // doesn't move in to canvas
+    {
+        if(newPatch->getIsNew()) // new generated one, but not put in the canvas
             activeContainer->removeChild(newPatch);
-        else
+        else{  // previous in canvas, need to be deleted
+            for (auto p = patchesQueue.begin(); p != patchesQueue.end();) {
+                if ((*p)->getID() == newPatch->getID()) {
+                patchesQueue.erase(p);
+                cout<<"size of patch queue : "<<patchesQueue.size()<<endl;
+                }else   p++; // fuck this line is very important, cost me 2h to find this bug!!!!!!!!!!
+            }
             mCanvas->removeChild(newPatch);
-        
+        }
         cout<<"removed child"<<endl;
     }
     
 }
 
-void ScrapWorkApp::mouseDown( MouseEvent event )
+void ScrapWorkApp::mouseUp(ci::app::MouseEvent event)
 {
-//
-//    if (mSelectPatchPanel->isInteractionEnabled()) {
-//        for (int i = 0 ; i < mSelectPatchPanel->getPatchNum() ; i++) {
-//            if (event.getX() > mSelectPatchPanel->getPatch(i)->getPosition().x
-//                && event.getX() < mSelectPatchPanel->getPatch(i)->getPosition().x + 50
-//                && event.getY() > mSelectPatchPanel->getPatch(i)->getPosition().y
-//                && event.getY() < mSelectPatchPanel->getPatch(i)->getPosition().y+50) {
-//                
-//                // if generate a new patch
-//                mSelectPatchPanel->setInteractionEnabled(false);
-//                newPatch = Patch::create(mSelectPatchPanel->getPatch(i)->getTexture());
-//                newPatch->setPosition(mSelectPatchPanel->getPatch(i)->getPosition());
-//            
-//                ci::app::timeline().apply(&newPatch->getScaleAnim(), newPatch->getScale(), newPatch->getScale()+ci::vec2(0.1), 0.3,ci::EaseOutQuad());
-//            
-//                newPatch->setNewOne(true);
-//            
-//                activeContainer->addChild(newPatch);
-//                
-//                }
-//            }
-//    }
+    ci::vec2 posInCanvas = ci::vec2(floor((event.getX() - 426.f)/100.f), floor((event.getY() - 259.f)/100.f));
+    cout <<"patch position is: ( "<<posInCanvas.x<<", "<<posInCanvas.y<<")"<<endl;
 }
 
-
-void ScrapWorkApp::mouseUp(MouseEvent event)
+void ScrapWorkApp::getPosInCanvas(int i, int j )
 {
-//    // only when hold a patch, select panel is interaction false
-//    if (!mSelectPatchPanel->isInteractionEnabled()) {
-//
-//        // if in play area
-//        if (event.getX() > 426 && event.getX() < 926 && event.getY() > 295 && event.getY() < 695)
-//        {
-//            // if the patch is a new one
-//            if(newPatch->isNew())
-//            {// if the patch is a new one and drag in the play area
-//                patchesQueue.push_back(newPatch);
-//                newPatch->setNewOne(false);
-//                
-//            }else
-//                ;// if the patch is not a new one, just change position, here is update position in position vector
-//            
-//        }
-//        
-//        // if not in play area, just fade out and delete child
-//        else
-//        {
-//            // if patch not in play area
-//            ci::app::timeline().apply(&newPatch->getAlphaAnim(),1.f ,0.f ,0.2 ,ci::EaseInSine());
-//        }
-//        cout<<"patchesQueue size : "<<patchesQueue.size()<<endl;
-//        mSelectPatchPanel->setInteractionEnabled(true);
-//    }
-//    // if not holding a patch
-//    else;
-    
-}
 
+}
 
 void ScrapWorkApp::update()
 {
